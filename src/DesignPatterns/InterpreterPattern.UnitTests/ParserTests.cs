e1fc7a4d41c6fe6c065a74b62b81abae8d64c7e1
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-01-23 16:33:47 +0100

    Dodanie przykładu wzorca interpreter na przykładzie odwrotnej notacji polskiej

--- src/DesignPatterns/InterpreterPattern.UnitTests/ParserTests.cs
+++ src/DesignPatterns/InterpreterPattern.UnitTests/ParserTests.cs
@@ -1,24 +1,31 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using FluentAssertions;
+using Xunit;
 
 namespace InterpreterPattern.UnitTests
 {
-    [TestClass]
     public class ParserTests
     {
-        [TestMethod]
-        public void Evaluate_ValidExpression_ShouldReturnResult()
+        private readonly Parser parser;
+
+        public ParserTests()
         {
-            // Arrange
-            string expression = "2 3 + 1 2 - *"; // (2 + 3) * (1 - 2) = -5
+            parser = new Parser(new ExpressionFactory());
+        }
 
-            var parser = new Parser();
 
+        [Theory]
+        [InlineData("2 2 +", 4)]
+        [InlineData("4 2 -", 2)]
+        [InlineData("2 2 *", 4)]
+        [InlineData("2 3 + 1 2 - *", -5)] // (2 + 3) * (1 - 2)
+        [InlineData("5 1 2 + 4 * + 3 -", 14)] // 5 + (1+2) × 4 − 3
+        public void Evaluate_ValidExpression_ShouldReturnResult(string expression, int expected)
+        {
             // Act
             int result = parser.Evaluate(expression);
 
             // Assert
-            Assert.AreEqual(-5, result);
-
+            result.Should().Be(expected);
         }
     }
 }
--- src/DesignPatterns/InterpreterPattern/Parser.cs
+++ src/DesignPatterns/InterpreterPattern/Parser.cs
@@ -1,12 +1,87 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace InterpreterPattern
 {
+    // Context
+    public class Context : Stack<int>
+    {
+
+    }
+
+    // Abstract Expression
+    public interface IExpression
+    {
+        void Interpret(Context context);
+    }
+
+
+    public class NumberExpression : IExpression
+    {
+        private readonly int number;
+        public NumberExpression(int number) => this.number = number;
+        public void Interpret(Context context) => context.Push(number);
+    }
+
+    public class PlusExpression : IExpression
+    {
+        public void Interpret(Context context) => context.Push(context.Pop() + context.Pop());
+    }
+
+    public class MinusExpression : IExpression
+    {
+        public void Interpret(Context context) => context.Push(-context.Pop() + context.Pop());
+    }
+
+    public class MultiplyExpression : IExpression
+    {
+        public void Interpret(Context context) => context.Push(context.Pop() * context.Pop());
+    }
+
+    public class ExpressionFactory
+    {
+        public IExpression Create(string token) =>
+            token switch
+            {
+                "+" => new PlusExpression(),
+                "-" => new MinusExpression(),
+                "*" => new MultiplyExpression(),
+                _ => new NumberExpression(int.Parse(token))
+            };
+    }
+
     public class Parser
     {
+        private IList<IExpression> parseTree = new List<IExpression>();
+
+        private readonly ExpressionFactory factory;
+
+        public Parser(ExpressionFactory factory) => this.factory = factory;
+
+        private string[] Tokenize(string s) => s.Split(' ');
+
+        private void Parse(string s)
+        {
+            var tokens = Tokenize(s);
+
+            foreach (var token in tokens)
+            {
+                parseTree.Add(factory.Create(token));
+            }
+        }
+
         public int Evaluate(string s)
         {
-            throw new NotImplementedException();
+            Parse(s);
+
+            var context = new Context();
+
+            foreach (var expression in parseTree)
+            {
+                expression.Interpret(context);
+            }
+
+            return context.Pop();
         }
     }
 
