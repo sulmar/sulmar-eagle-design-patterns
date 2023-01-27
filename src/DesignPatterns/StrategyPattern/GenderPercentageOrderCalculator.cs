654c03adc276c39f9914a41cdcdef20a358ac2cb
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-01-27 13:59:32 +0100

    Przeniesienie kodu

--- src/DesignPatterns/StrategyPattern/GenderPercentageOrderCalculator.cs
+++ src/DesignPatterns/StrategyPattern/GenderPercentageOrderCalculator.cs
@@ -2,51 +2,7 @@
 
 namespace StrategyPattern
 {
-    public interface IBankomat
-    {
-        void Wplata(decimal amount);
-        void Wyplata(decimal amount);
-    }
-
-    public interface IWplatomat
-    {
-        void Wplata(decimal amount);
-    }
-
-    public interface IWyplatomat
-    {
-        void Wyplata(decimal amount);
-        
-    }
-
-    public class SantanderBankomat : IWyplatomat, IWplatomat
-    {
-        public void Wplata(decimal amount)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Wyplata(decimal amount)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
-    public class PkoBankomat : IBankomat
-    {
-        decimal saldo;
-
-        public void Wplata(decimal amount)
-        {
-            throw new NotSupportedException();
-        }
-
-        public void Wyplata(decimal amount)
-        {
-            saldo -= amount;            
-        }
-    }
-
+    
     public interface IDiscountStrategy
     {
         bool CanDiscount(Order order);
